00ac9cfbd87ea4b770431c8200deab27a2afa37c
HuMiaoxin666 <HuMiaoxin66@gamil.com>
2019-03-21 11:37:36 +0800

    Add kmeans!

---
+++ public/js/my/axis.js
@@ -0,0 +1,120 @@
+let axisChart = (function () {
+    let sankey_width = $('#svg_sankey')[0].scrollWidth;
+    let sankey_height = $('#svg_sankey')[0].scrollHeight;
+    function drawAxis(comb_data) {
+        variable.svg_sankey.selectAll('circle').remove();
+        let loc_arr = [];
+
+        for (let key in comb_data['loc']) {
+            tmp_node = {};
+            tmp_node['loc'] = [parseFloat(comb_data['loc'][key][0]), parseFloat(comb_data['loc'][key][1])];
+            tmp_node['db_clu'] = key;
+            tmp_node['k_clu'] = comb_data['id_k_clu'][key];
+            loc_arr.push(tmp_node)
+        }
+        console.log('loc_arr: ', loc_arr);
+        let min_x = d3.min(loc_arr, d => d.loc[0]);
+        let min_y = d3.min(loc_arr, d => d.loc[1]);
+        let max_x = d3.max(loc_arr, d => d.loc[0]);
+        let max_y = d3.max(loc_arr, d => d.loc[1]);
+        let xScale = d3.scaleLinear().domain([min_x, max_x]).range([30, sankey_width / 2 - 30])
+        let yScale = d3.scaleLinear().domain([min_y, max_y]).range([sankey_height - 15, 15])
+        let color = d3.scaleOrdinal(d3.schemeCategory20);
+
+        variable.svg_sankey.append('g').selectAll('circle').data(loc_arr).enter()
+            .append('circle')
+            .attr('cx', d => xScale(d.loc[0]))
+            .attr('cy', d => yScale(d.loc[1]))
+            .attr('fill', d => color(d.k_clu))
+            .attr('stroke', d => color(d.k_clu))
+            .attr('r', 5)
+            .on('click', function (d) {
+                let clu_ids = comb_data['k_clu_ids'][d.k_clu];
+                let links = [], node_dict = {}, nodes = [];
+                for (let i = 0; i < clu_ids.length; i++) {
+                    for (let j = 0; j < comb_data.clu_tpg[clu_ids[i]].length; j++) {
+                        let tmp_link = comb_data.clu_tpg[clu_ids[i]][j];
+                        links.push({ 'source': tmp_link[0], 'target': tmp_link[1] })
+                        if (node_dict[tmp_link[0]] == undefined)
+                            node_dict[tmp_link[0]] = true;
+                        if (node_dict[tmp_link[1]] == undefined)
+                            node_dict[tmp_link[1]] = true;
+                    }
+                }
+
+                for (let key in node_dict) {
+                    nodes.push({ 'id': key })
+                }
+                drawTopo(nodes, links);
+            })
+    }
+
+    function drawTopo(nodes, links) {
+        variable.svg_scatter.selectAll('*').remove();
+
+        let forceClusterWidth = $('#svg_scatter')[0].scrollWidth;
+        let forceClusterHeight = $('#svg_scatter')[0].scrollHeight;
+
+       
+        let simulation_cluster = d3.forceSimulation(nodes)
+            .force("charge", d3.forceManyBody().distanceMax(50))
+            .force("link", d3.forceLink(links).id(d => d.id))
+            .force("center", d3.forceCenter(forceClusterWidth / 2, forceClusterHeight / 2))
+        let drag_cluster = simulation_cluster => {
+
+            function dragstarted(d) {
+                if (!d3.event.active) simulation_cluster.alphaTarget(0.3).restart();
+                d.fx = d.x;
+                d.fy = d.y;
+            }
+
+            function dragged(d) {
+                d.fx = d3.event.x;
+                d.fy = d3.event.y;
+            }
+
+            function dragended(d) {
+                if (!d3.event.active) simulation_cluster.alphaTarget(0);
+                d.fx = null;
+                d.fy = null;
+            }
+
+            return d3.drag()
+                .on("start", dragstarted)
+                .on("drag", dragged)
+                .on("end", dragended);
+        }
+
+        //设置tick函数
+        simulation_cluster.on("tick", () => {
+            link_cluster
+                .attr("x1", d => d.source.x)
+                .attr("y1", d => d.source.y)
+                .attr("x2", d => d.target.x)
+                .attr("y2", d => d.target.y);
+
+            node_cluster
+                .attr("cx", d => d.x)
+                .attr("cy", d => d.y);
+
+        })
+        //画线
+        let link_cluster = variable.svg_scatter.append('g').selectAll('line').data(links).enter()
+            .append('line')
+            .attr('stroke', '#999')
+            .attr('opacity', 0.5)
+            .attr('stroke-width', 2)
+        //画点
+        let node_cluster = variable.svg_scatter.append('g').selectAll('circle').data(nodes).enter()
+            .append('circle')
+            .attr('r', 3)
+            .attr('stroke', '#b4b4ff')
+            .attr('fill', '#b4b4ff')
+            .call(drag_cluster(simulation_cluster));
+
+
+    }
+    return {
+        drawAxis
+    }
+})()
--- public/js/my/option.js
+++ public/js/my/option.js
@@ -8,7 +8,7 @@ let option = (function () {
     let tmp_param = ["cited", '次', 'cited_chart'];
     let wt = 'wt_3', sl = 'sl_5', rl = 'rl_False';
     //读取数据和初始化各窗口
-    d3.json('data/all_param_k_clu.json', function (all_data) {
+    d3.json('data/all_param_k_clu_5.json', function (all_data) {
         console.log('all_data: ', all_data);
         //赋值总数据集合
         variable.all_data = all_data;
@@ -107,46 +107,8 @@ let option = (function () {
         variable.cluster_dict = tmp_dict;;
         variable.cluster_record.push(tmp_dict);
         scatter.drawScatter(tmpCombData);
+        axisChart.drawAxis(variable.comb_data);
         ForceChart.Clustering(variable.comb_data['clu_ids'], variable.comb_data['cluster_link'], variable.cluster_dict);
-
-//**********判断是否可以绘制sankey**************
-
-        let tmp_index = variable.confirm_time;//当前操作下标
-
-        let link_value_dict = {}; //便于计算每条link的value
-        let max_cluster = -10000;//获取当前操作的最大簇
-        console.log('cluster_record: ', variable.cluster_record);
-        for (let id in variable.cluster_record[tmp_index - 1]) {
-            let source_cluster = variable.cluster_record[tmp_index - 1][id];//该点的起始簇
-            let target_cluster = variable.cluster_record[tmp_index][id];//该点的目标簇
-            if (target_cluster > max_cluster)
-                max_cluster = target_cluster;
-            //判断簇的始末位置都不是噪音簇
-            if (source_cluster != -1 && target_cluster != -1 && source_cluster != undefined && target_cluster != undefined) {
-                let tmp_key = source_cluster + '_' + target_cluster;
-                if (link_value_dict[tmp_key])
-                    link_value_dict[tmp_key] += 1;
-                else
-                    link_value_dict[tmp_key] = 1;
-            }
-        }
-        //将每种link组合转换成arr形式
-        for (let key in link_value_dict) {
-            let source = (tmp_index - 1) + '_' + key.split('_')[0];
-            let target = tmp_index + '_' + key.split('_')[1];
-            let tmp_link = { 'source': source, 'target': target, 'value': link_value_dict[key], 'id': source + '_' + target };
-            //将当前操作的连线数据加入
-            variable.sankeyLink_data.push(tmp_link)
-        }
-        for (let i = 0; i <= max_cluster; i++) {
-            let tmp_id = tmp_index + '_' + i;
-            variable.sankeyNode_data.push({ 'id': tmp_id });
-        }
-        //调用桑基图绘制函数
-        if (tmp_index > 0)
-            sankeyChart.drawSankey(variable.sankeyNode_data, variable.sankeyLink_data);
-        //将最大簇修改
-        $('#max_cluster').text(max_cluster);
     })
 
     return {
